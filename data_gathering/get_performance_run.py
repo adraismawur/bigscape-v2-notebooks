e11dbf54315fd26acc92ba9fc03b25e2d0dec151
Arjan Draisma <arjan.draisma@wur.nl>
2024-12-05 18:26:00 +0100

    rework performance stuff

--- data_gathering/get_performance_run.py
+++ data_gathering/get_performance_run.py
@@ -3,25 +3,37 @@ from datetime import datetime
 from pathlib import Path
 
 
-def print_profile_matrix_line(version, parts, start, type="MULTI", adjust_mem_subprocesses=False):
-
-    if parts[1] != type:
-        return
+def print_profile_matrix_line_v1(version, parts, start):
 
     time = datetime.fromisoformat(parts[0])
     seconds = (time - start).total_seconds()
 
-    cpu = float(parts[2])
-    processes = int(parts[3])
-    mem_used_mb = float(parts[4])
+    # cpu = float(parts[2]) if parts[1] != "MULTI" else 0.0
+    # processes = int(parts[3]) if parts[1] == "MULTI" else 0
+    # mem_used_mb = float(parts[4]) if parts[1] == "MAIN" else 0.0
 
-    # there seems to be a difference between the multiprocessing used in v1 vs
-    # v2. in v2, the memory appears accurate on the multi type log, but in v2 this
-    # appears summed for all processes. so we need to adjust this.
-    if adjust_mem_subprocesses:
-        mem_used_mb /= processes
+    cpu = float(parts[2]) if parts[1] != "MULTI" else 0.0
+    processes = int(parts[3]) if parts[1] == "MULTI" else 0
+    mem_used_mb = float(parts[4]) if parts[1] == "MAIN" else 0.0
 
     memused_percent = float(parts[5])
+
+    print(f"{version},{seconds},{cpu},{processes},{mem_used_mb},{memused_percent}")
+
+
+def print_profile_matrix_line_v2(version, parts, start):
+    time = datetime.fromisoformat(parts[0])
+    seconds = (time - start).total_seconds()
+
+    cpu = float(parts[2]) if parts[1] != "MULTI" else 0.0
+    processes = int(parts[3])
+    mem_used_mb = float(parts[4]) if parts[1] == "MULTI" else 0.0
+
+    # cpu = float(parts[2]) if "CHILD" in parts[1] else 0.0
+    # processes = int(parts[3])
+    # mem_used_mb = float(parts[4]) if parts[1] == "MAIN" else 0.0
+
+    memused_percent = float(parts[5]) if parts[1] == "MAIN" else 0.0
 
     print(f"{version},{seconds},{cpu},{processes},{mem_used_mb},{memused_percent}")
 
@@ -30,8 +42,6 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("v1_folder", type=Path)
     parser.add_argument("v2_folder", type=Path)
-
-    parser.add_argument("--type", default="MULTI")
 
     args = parser.parse_args()
 
@@ -47,25 +57,22 @@ if __name__ == "__main__":
     with open(v1_profile) as f:
         # skip header
         f.readline()
-        
+
         v1_start = datetime.fromisoformat(f.readline().split(",")[0])
 
         for line in f:
             parts = line.split(",")
 
-            print_profile_matrix_line("v1", parts, v1_start, args.type)
+            print_profile_matrix_line_v1("v1", parts, v1_start)
 
-    
     v2_start: datetime
     with open(v2_profile) as f:
         # skip header
         f.readline()
-        
+
         v2_start = datetime.fromisoformat(f.readline().split(",")[0])
 
         for line in f:
             parts = line.split(",")
 
-            print_profile_matrix_line("v2", parts, v2_start, args.type, True)
-
-
+            print_profile_matrix_line_v2("v2", parts, v2_start)
